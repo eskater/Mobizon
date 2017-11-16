938ddef76b5b259d4db01f9b9a81513914aefb3a
laraketai <g.astakhoff@gmail.com>
2017-11-17 04:19:02 +0600

    Update MobizonServiceProvider.php

--- src/MobizonServiceProvider.php
+++ src/MobizonServiceProvider.php
@@ -3,15 +3,31 @@
 namespace Laraketai\Mobizon;
 
 use Illuminate\Support\ServiceProvider;
+use Mobizon\MobizonApi;
+
 
 class MobizonServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/mobizon.php' => config_path('mobizon.php') . "/"
+        ], 'config');
+    }
+
     public function register()
     {
-        $this->app->singleton(MobizonApi::class, function () {
-            $config = config('services.mobizon');
+        $this->app->singleton(Mobizon::class, function () {
+            $config = config('mobizon');
 
-            return new MobizonApi($config['alphaname'], $config['secret']);
+            return new Mobizon($config['secret']);
+        });
+
+        $this->app->singleton(MobizonApi::class, function () {
+            $config = config('mobizon');
+
+            return new MobizonApi($config['secret']);
         });
     }
+
 }
